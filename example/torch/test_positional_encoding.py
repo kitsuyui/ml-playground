d50bb588ace353a73195b20fc6666c0a16636ae0
kitsuyui <kitsuyui@kitsuyui.com>
2023-02-08 22:43:39 +0900

    Add example of positional encoding with pytorch

---
+++ example/torch/test_positional_encoding.py
@@ -0,0 +1,16 @@
+import torch
+
+from example.torch.positional_encoding import PositionalEncoding
+
+
+def test_positional_encoding() -> None:
+    pe = PositionalEncoding(d_model=4, dropout=0.0)
+    x = torch.Tensor(
+        [1.0, 2.0, 3.0, 4.0],
+    )
+    y = pe(x)
+    diff = y - x
+    # The difference should be between -1.0 and 1.0
+    assert torch.allclose(
+        diff, torch.Tensor([0.0, 0.0, 0.0, 0.0]), rtol=0.0, atol=1.0
+    )
