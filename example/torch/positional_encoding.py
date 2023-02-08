6552c41b6af57f88a72f311fa17465e28b1b43a5
kitsuyui <kitsuyui@kitsuyui.com>
2023-02-08 23:16:35 +0900

    Add other Positional Encoding definition

--- example/torch/positional_encoding.py
+++ example/torch/positional_encoding.py
@@ -1,4 +1,5 @@
 import math
+from typing import OrderedDict
 
 import torch
 import torch.nn as nn
@@ -36,3 +37,58 @@ class PositionalEncoding(nn.Module):
         """
         x = x + self.pe[: x.size(0)]  # type: ignore
         return self.dropout(x)  # type: ignore
+
+
+class RawPositionalEncoding(nn.Module):
+    """PositionalEncoding without the dropout layer."""
+
+    def __init__(
+        self,
+        d_model: int,
+        max_len: int = 5000,
+    ):
+        super().__init__()
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model)
+        )
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Args:
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+        x = x + self.pe[: x.size(0)]  # type: ignore
+        return x  # type: ignore
+
+
+class PositionalEncoding2(nn.Module):
+    def __init__(
+        self,
+        d_model: int,
+        dropout: float = 0.1,
+        max_len: int = 5000,
+    ):
+        super().__init__()
+        self.seq = nn.Sequential(
+            OrderedDict(
+                [
+                    ("dropout", nn.Dropout(p=dropout)),
+                    (
+                        "pe",
+                        RawPositionalEncoding(d_model, max_len),
+                    ),
+                ]
+            )
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Args:
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+        return self.seq(x)  # type: ignore
--- example/torch/test_positional_encoding.py
+++ example/torch/test_positional_encoding.py
@@ -1,6 +1,11 @@
+import timeit
+
 import torch
 
-from example.torch.positional_encoding import PositionalEncoding
+from example.torch.positional_encoding import (
+    PositionalEncoding,
+    PositionalEncoding2,
+)
 
 
 def test_positional_encoding() -> None:
@@ -11,3 +16,39 @@ def test_positional_encoding() -> None:
     y = pe(x)
     # The difference should be between -1.0 and 1.0
     assert torch.allclose(x, y, rtol=0.0, atol=1.0)
+
+
+def test_positional_encoding_repr() -> None:
+    pe = PositionalEncoding(d_model=4, dropout=0.0)
+    assert (
+        repr(pe)
+        == """\
+PositionalEncoding(
+  (dropout): Dropout(p=0.0, inplace=False)
+)"""
+    )
+    pe2 = PositionalEncoding2(d_model=4, dropout=0.0)
+    assert (
+        repr(pe2)
+        == """\
+PositionalEncoding2(
+  (seq): Sequential(
+    (dropout): Dropout(p=0.0, inplace=False)
+    (pe): RawPositionalEncoding()
+  )
+)"""
+    )
+    x = torch.Tensor(
+        [1.0, 2.0, 3.0, 4.0],
+    )
+    y = pe(x)
+    y2 = pe2(x)
+    # same result
+    assert torch.allclose(y, y2, rtol=0.0, atol=0.0)
+
+    # benchmark
+    t0 = timeit.timeit("pe(x)", globals=locals(), number=1000)
+    t1 = timeit.timeit("pe2(x)", globals=locals(), number=1000)
+
+    # the original implementation is faster because it is simpler (it does not use Sequential module)
+    assert t0 < t1
